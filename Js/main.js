1db78d2b5584c427b964ea45a508c36acea2d5ec
Hamed <ha5506569@gmail.com>
2026-08-03 16:21:25 +0300

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -13,7 +13,7 @@ function login() {
 
     const pass = document.getElementById("password").value;
 
-    if (user === "Nouran" && pass === "2010") {
+    if (user === "Nouran" && pass === "2010&2008") {
 
         goToPage(2)
 
